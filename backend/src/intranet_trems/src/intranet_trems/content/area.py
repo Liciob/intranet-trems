442988c321d106aa24b2a7034e62b171715fc678
Lício Brito <liciob@gmail.com>
2024-05-23 16:45:11 -0400

    Exercício 46

--- backend/src/intranet_trems/src/intranet_trems/content/area.py
+++ backend/src/intranet_trems/src/intranet_trems/content/area.py
@@ -1,3 +1,5 @@
+from intranet_trems import _
+from plone import api
 from plone.dexterity.content import Container
 from plone.supermodel.model import Schema
 from zope import schema
@@ -7,15 +9,16 @@ from zope.interface import implementer
 class IArea(Schema):
     """Definição de uma Área no TRE-MS."""
 
-    # Informações básicas
-    title = schema.TextLine(title="Nome da Área", required=True)
-    description = schema.Text(title="Descrição", required=False)
-
-    # Contato
-    email = schema.TextLine(title="E-mail", required=True)
-    ramal = schema.TextLine(title="Ramal", required=True)
+    title = schema.TextLine(title=_("Nome da Área"), required=True)
+    description = schema.Text(title=_("Descrição"), required=False)
 
 
 @implementer(IArea)
 class Area(Container):
     """Área no TRE-MS."""
+
+    @property
+    def pessoas(self):
+        """Lista de pessoas conectadas a esta área."""
+        relations = api.relation.get(target=self, relationship="area")
+        return [i.from_object for i in relations]
